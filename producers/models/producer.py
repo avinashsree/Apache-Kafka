1097d5d47f25c7fa37ba78d16839924d093590d0
Avinash <apple@apples-MacBook-Air.local>
2021-01-26 16:03:40 +0530

    Added the config details for Kafka Producer while creating the topic

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -2,26 +2,27 @@
 import logging
 import time
 
+
 from confluent_kafka import avro
 from confluent_kafka.admin import AdminClient, NewTopic
 from confluent_kafka.avro import AvroProducer, CachedSchemaRegistryClient
 
 logger = logging.getLogger(__name__)
 
-
 class Producer:
     """Defines and provides common functionality amongst Producers"""
 
     # Tracks existing topics across all Producer instances
     existing_topics = set([])
+    BROKER_URL = "PLAINTEXT://localhost:9092"
 
     def __init__(
-            self,
-            topic_name,
-            key_schema,
-            value_schema=None,
-            num_partitions=1,
-            num_replicas=1,
+        self,
+        topic_name,
+        key_schema,
+        value_schema=None,
+        num_partitions=1,
+        num_replicas=1,
     ):
         """Initializes a Producer object with basic settings"""
         self.topic_name = topic_name
@@ -30,8 +31,14 @@ class Producer:
         self.num_partitions = num_partitions
         self.num_replicas = num_replicas
 
+        #
+        #
+        # TODO: Configure the broker properties below. Make sure to reference the project README
+        # and use the Host URL for Kafka and Schema Registry!
+        #
+        #
         self.broker_properties = {
-            'bootstrap.servers': 'PLAINTEXT://localhost:9092'
+            "bootstrap.servers": self.BROKER_URL
         }
 
         # If the topic does not already exist, try to create it
@@ -39,20 +46,55 @@ class Producer:
             self.create_topic()
             Producer.existing_topics.add(self.topic_name)
 
-        self.producer = AvroProducer(self.broker_properties,
-                                     schema_registry=CachedSchemaRegistryClient('http://localhost:8081'),
-                                     default_key_schema=self.key_schema,
-                                     default_value_schema=self.value_schema
-                                     )
+        # TODO: Configure the AvroProducer
+        self.producer = AvroProducer(self.broker_properties, schema_registry=CachedSchemaRegistryClient("http://localhost:8081"))
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
-        AdminClient(self.broker_properties).create_topics([NewTopic(self.topic_name, num_partitions=1)])
+        #
+        #
+        # TODO: Write code that creates the topic for this producer if it does not already exist on
+        # the Kafka Broker.
+        client = AdminClient(self.broker_properties)
+        futures = client.create_topics(
+            [
+                NewTopic(
+                    topic=self.topic_name,
+                    num_partitions=self.num_partitions,
+                    replication_factor=self.num_replicas,
+                    config={
+                        "cleanup.policy": "delete",
+                        "compression.type": "lz4",
+                        "delete.retention.ms": "2000",
+                        "file.delete.delay.ms": "2000",
+                    },
+                )
+            ]
+        )
+        topic_metadata = client.list_topics(timeout=5)
+        topic_exists = self.topic_name in set(t.topic for t in iter(topic_metadata.topics.values()))
+        if not topic_exists:
+            for topic, future in futures.items():
+                try:
+                    future.result()
+                    logger.info("topic created")
+                except Exception as e:
+                    logger.error(f"failed to create topic {self.topic_name}: {e}")
+        else:
+            logger.info(f"{self.topic_name} topic already created")
+
+    def time_millis(self):
+        return int(round(time.time() * 1000))
 
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
+        #
+        #
+        # TODO: Write cleanup code for the Producer here
+        #
         if self.producer is not None:
             self.producer.flush()
+        logger.info("producer close incomplete - skipping")
 
     def time_millis(self):
         """Use this function to get the key for Kafka Events"""
